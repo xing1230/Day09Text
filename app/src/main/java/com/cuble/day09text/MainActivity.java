977ca41937b609feaeeeda3ba9576dba4a660062
xing1230 <691169173@qq.com>
2016-06-30 15:56:50 +0800

    这次的提交

---
+++ app/src/main/java/com/cuble/day09text/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cuble.day09text;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
