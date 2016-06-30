52eba0494aea923f4d0f848baf0a2e2be8278e15
xing1230 <691169173@qq.com>
2016-06-30 16:06:29 +0800

    这次的提交再次

--- app/src/main/java/com/cuble/day09text/MainActivity.java
+++ app/src/main/java/com/cuble/day09text/MainActivity.java
@@ -3,9 +3,11 @@ package com.cuble.day09text;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     Button bt;
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
